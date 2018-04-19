9c5d4d2311d107b2064e5f1430915e2661ec3609
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-04-19 20:58:21 +0400

    --

--- pages/camera/home-camera.js
+++ pages/camera/home-camera.js
@@ -1,25 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Text, View, TouchableOpacity, Button } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 export class HomeCameraScreen extends React.Component {
     static navigationOptions = {
-        drawerLabel: 'Home Camera',
         title: 'Home Camera',
+    };
+    state = {
+        hasCameraPermission: null,
+        type: Camera.Constants.Type.back,
+    };
+
+    async componentWillMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
+    }
+
+    snap = async () => {
+        if (this.camera) {
+
+
+
+            let photo = await this.camera.takePictureAsync();
+
+            alert(photo);
+        }
     };
 
     render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home Camera Screen</Text>             
-
-                <Button
-                    title="Menu"
-                    onPress={() => {
-                        this.props.navigation.navigate('DrawerOpen');
-                    }}
-                />
-
-            </View>
-        );
+        const { hasCameraPermission } = this.state;
+        if (hasCameraPermission === null) {
+            return <View />;
+        } else if (hasCameraPermission === false) {
+            return <Text>No access to camera</Text>;
+        } else {
+            return (
+                <View style={{ flex: 0.5 }}>
+                    <Camera style={{ flex: 1 }} type={this.state.type} ref={ref => { this.camera = ref; }}>
+                        <View
+                            style={{
+                                flex: 1,
+                                backgroundColor: 'transparent',
+                                flexDirection: 'row',
+                            }}>
+                            <TouchableOpacity
+                                style={{
+                                    flex: 0.1,
+                                    alignSelf: 'flex-end',
+                                    alignItems: 'center',
+                                }}
+                                onPress={() => {
+                                    this.setState({
+                                        type: this.state.type === Camera.Constants.Type.back
+                                            ? Camera.Constants.Type.front
+                                            : Camera.Constants.Type.back,
+                                    });
+                                }}>
+                                <Text
+                                    style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                                    {' '}Flip{' '}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Camera>
+                    <Button
+                        title="Get Picture"
+                        onPress={() => {
+                            this.snap();
+                        }}
+                    />
+                </View>
+            );
+        }
     }
 }
