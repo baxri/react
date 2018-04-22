3d75f838d27b3d2ac7af5cba1b456c7cdb3941c3
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-04-23 01:35:44 +0400

    --

--- database.js
+++ database.js
@@ -2,29 +2,31 @@ import PouchDB from 'pouchdb-react-native';
 
 let localdb = new PouchDB('testDB');
 
-export const countDocs = () => new Promise((resolve,reject) => {
-  queryDocs().then(items => resolve(items.length)).catch(error=>reject(error));
+export const countDocs = () => new Promise((resolve, reject) => {
+    queryDocs().then(items => resolve(items.length)).catch(error => reject(error));
 });
-export const addDoc = (data) => new Promise((resolve,reject) => {
-  const newDoc = {
-    _id: Math.floor(Date.now()/1000).toString(),
-    text:data,
-    additionDate: new Date(),
-  };
-  localdb.put(newDoc).then(result => {
-    console.log("Inserted");
-    resolve(result);
-  }).catch(error => {
-    console.error("Error:", error.name);
-    reject(error);
-  });
+export const addDoc = (data) => new Promise((resolve, reject) => {
+    const newDoc = {
+        _id: Math.floor(Date.now() / 1000).toString(),
+        name: data,
+    };
+
+    localdb.put(newDoc).then(result => {
+        console.log("Inserted");
+        resolve(result);
+    }).catch(error => {
+        console.error("Error:", error.name);
+        reject(error);
+    });
 });
 
-export const queryDocs = () => new Promise((resolve,reject) => {
-    localdb.allDocs({include_docs: true, limit: null}).then(result => {
-    const items = result.rows.map(row => row.doc);
-    console.log("RESULT:",items);
-    resolve(items);
-  }).catch(error => reject(error));
+
+
+export const queryDocs = () => new Promise((resolve, reject) => {
+    localdb.allDocs({ include_docs: true, limit: null }).then(result => {
+        const items = result.rows.map(row => row.doc);
+        console.log("RESULT:", items);
+        resolve(items);
+    }).catch(error => reject(error));
 })
 export default localdb;
