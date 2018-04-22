d29df822540048e73d41231c107dfda0607f3ae3
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-04-22 23:18:37 +0400

    --

--- pages/home-crud/edit-crud.js
+++ pages/home-crud/edit-crud.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 export class EditCrudScreen extends React.Component {
     constructor(props) {
         super(props)
+
+        const { params } = this.props.navigation.state;
+
+        this.state.id = params ? params.id : 0;
+        this.state.name = params ? params.name : '';
+
     }
 
     static navigationOptions = {
@@ -20,22 +26,12 @@ export class EditCrudScreen extends React.Component {
         alert(this.state.name);
     }
 
-    handleChange(e) {
-        this.setState({ name: e.target.value });
-    }
-
     render() {
-
-        const { params } = this.props.navigation.state;
-
-        this.state.id = params ? params.id : 0;
-        this.state.name = params ? params.name : '';
-
         return (
             <View style={{ flex: 1 }}>
                 <FormLabel>Name</FormLabel>
-                <FormInput
-                    onChange={this.handleChange.bind(this)}
+                <TextInput
+                    onChangeText={(name) => this.setState({ name })}
                     value={this.state.name}
                 />
 
